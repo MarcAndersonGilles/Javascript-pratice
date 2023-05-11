d8413c2eef34e831371f66e7b682a60dcad689f8
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-05-11 10:16:39 -0400

    json testing to see how it works

---
+++ components/jsonTest/Monster.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function monster() {
+    const monster = `[ {
+        "name": "Bulbasaur",
+        "type": "Grass",
+        "hp": 45,
+        "attack": 49,
+        "defense": 49,
+        "spAttack": 65,
+        "spDefense": 65,
+        "speed": 45,
+        "moves": [
+            "Tackle",
+            "Vine Whip",
+            "Razor Leaf",
+            "Seed Bomb"
+        ]
+    }]`
+    console.log(JSON.parse(monster))
+  return (
+    <div className=''>
+        <h1>{monster}</h1>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,15 @@ import { Inter } from 'next/font/google'
 import Input from '../components/Input'
 import Map from '../components/Map'
 import States from '../components/useState/States'
+import Monster from '@/components/jsonTest/monster'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Map />
-      <States />
+      {/* <Map />
+      <States /> */}
+      <Monster />
     </main>
   )
 }

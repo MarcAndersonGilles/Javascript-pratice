823de044269e5ac78f269b3c5e3196fd4dd44596
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-06-02 18:03:42 -0400

    porfolio debut

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Input from '../components/Input'
-import Map from '../components/Map'
-import States from '../components/useState/States'
-import Monster from '@/components/jsonTest/Monster'
-import UseEffectFun from '@/components/useState/UseEffectFun'
-import UseRef from '@/components/useRef/UseRef'
-import Promises from '@/components/promises/Promises'
-import AsyncAwait from '@/components/promises/AsyncAwait'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Map />
-      <States /> */}
-      {/* <Monster /> */}
-      {/* { <UseEffectFun /> } */}
-      {/* <UseRef /> */}
-      {/* <Promises /> */}
-      <AsyncAwait />
+     
+     
     </main>
   )
 }
